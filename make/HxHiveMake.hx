dec6018405b2b634b259570bc036dc4aae1242ca
vitaliyk <vitaliyk@playtika.com>
2017-09-26 23:57:53 +0300

    removed useless test task

--- make/HxHiveMake.hx
+++ make/HxHiveMake.hx
@@ -1,7 +1,5 @@
 package ;
 
-import hxmake.haxelib.HaxelibExt;
-import hxmake.test.TestTask;
 import hxmake.idea.IdeaPlugin;
 import hxmake.haxelib.HaxelibPlugin;
 
@@ -12,31 +10,11 @@ class HxHiveMake extends hxmake.Module {
 		config.classPath = ["src"];
 		config.testPath = ["test"];
 		config.devDependencies = [
-			"utest" => "haxelib",
 			"hxdispose" => "haxelib",
 			"hxfireflies" => "haxelib"
 		];
 
 		apply(HaxelibPlugin);
 		apply(IdeaPlugin);
-
-		library(
-			function(ext:HaxelibExt) {
-				ext.config.description = "particles editor, based on hxfireflies";
-				ext.config.contributors = ["storm_ex"];
-				ext.config.url = "https://github.com/StormEx/hxhive";
-				ext.config.license = "MIT";
-				ext.config.version = "0.0.1";
-				ext.config.releasenote = "Initial release";
-				ext.config.tags = ["particles editor"];
-
-				ext.pack.includes = ["src", "haxelib.json", "README.md"];
-			}
-		);
-
-		var tt = new TestTask();
-		tt.targets = ["neko", "swf", "js", "node", "cpp", "java", "cs", "php"];
-		tt.libraries = ["hxfireflies", "hxdispose", "hxhive"];
-		task("test", tt);
 	}
 }
